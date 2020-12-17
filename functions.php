46cc5a58829b3ea5f0c6879942b05c59524a6fc2
EmmyKrist <emmalina.krist@wsu.edu>
2020-12-17 21:50:49 +0000

    added Tor's database code to functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,28 @@
 <?
+
+
+require 'database.php';
+
+$requestedID = filter_var(htmlspecialchars($_GET["id"]), FILTER_SANITIZE_STRING);
+
+$requestedList = filter_var(htmlspecialchars($_GET["list"]), FILTER_SANITIZE_STRING);
+
+$sql = "SELECT * FROM $requestedList
+WHERE name='$requestedID'
+ORDER BY sequence";
+$requestedOutput = $db->query($sql);
+if (!$requestedOutput) die("List Error: " . $sql . "<br>" . $db->error);
+
+$requestedJSON = "0";
+
+if ($requestedOutput->num_rows > 0) {
+$outputArray = [];
+while( $row = $requestedOutput->fetch_assoc() ) {
+$outputArray[] = $row;
+}
+$requestedJSON = json_encode($outputArray);
+}
+
+echo $requestedJSON;
+
+
